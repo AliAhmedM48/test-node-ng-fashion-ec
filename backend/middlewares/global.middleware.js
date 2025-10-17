4243cf7e070830e47ba71b9fccc1d0ad38f4abfc
Ali Ahmed <MrAliAhmedM@gmail.com>
2025-10-17 18:14:37 +0300

    first commit

---
+++ backend/middlewares/global.middleware.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+dotenv.config();
+
+module.exports = [
+    express.json(),
+    express.urlencoded(),
+    cookieParser(),
+    cors(
+        {
+        origin: ["http://localhost:4200", "https://fashion-platform-so1s.vercel.app"],  
+        credentials: true
+        }
+    ),
+    morgan('dev'),
+]
+
